a140559f9317df34fc22a34ac1e36d3bc3447500
Andrew Xing <xingandrew1@gmail.com>
2017-09-15 23:01:04 -0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -15,13 +15,15 @@ $(document).ready(function(){
 
 	$('.button').on('click', function() {
 		var $input = $('input[name=todo]').val();
-		$('.filler').append('<div class="space">' + $input + '</div>');
-		$('.danktain').show();
+		if ($input != "") {
+			$('.filler').append('<div class="space">' + $input + '</div>');
+			$('.danktain').show();
+			
+			localStorage.setItem(i, $('input[name=todo]').val());
+			i += 1;
 
-		localStorage.setItem(i, $('input[name=todo]').val());
-		i += 1;
-
-		$('input[name=todo]').val("");
+			$('input[name=todo]').val("");
+		}
 	});
 
 	$('.dankform').submit(function() {
@@ -57,6 +59,10 @@ $(document).ready(function(){
 		
 	});
 
+	$('#clear').on('click', function() {
+		localStorage.clear(); 
+		location.reload(); //Took the easy way around ^_^
+	});
 	//We should add local storage to this! Local storage with appending is easier, but updating local storage 6132614324
 	//after removing something is a bit trickier
 	//We must shift the storage values of everything after the element removed up 1 (eg, 3 --> 2)
